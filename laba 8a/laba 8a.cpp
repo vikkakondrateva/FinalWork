a1c3c5d0ae3b18ced5cdf40ea985abab7bde030b
Vika <vikka_kondrateva@mail.ru>
2023-05-23 15:32:45 +0300

    8

---
+++ laba 8a/laba 8a.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void inputArray(int* array, int size);// обявляем прототип функции(прототип-информация для компилятора)
+void outputArray(int* array, int size);
+double processArray(int* array, int size);
+void outputResult(double result);
+
+//#define SIZE 10 //константа
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int m[10];
+    double res;
+  
+    inputArray(m, 10); //вызываются
+    outputArray(m, 10);
+    res = processArray(m, 10);
+    outputResult(res);
+
+    return 0;
+}
+
+void inputArray(int* array, int size)//заголовок функции: тип возвращ знач, имя функции, параметры
+{
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        *array = rand();
+        array++;
+    }
+}
+
+void outputArray(int* array, int size) // void-функция не возвращ знач(пустой тип)
+{
+    printf("Массив случайных чисел: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", *array);
+        array++;
+    }
+}
+
+double processArray(int* array, int size)
+{
+    double g = 1;
+    for (int i = 0; i < size; i++)
+    {
+        g = g * (*array);
+        array++;
+    }
+    g = pow(g, 0.1);
+    return g;//возвращаемое значение
+}
+
+void outputResult(double result)
+{
+    printf("Среднее геометрическое массива: \n");
+    printf("%lf\n", result);
+}
---
+++ laba 8b/laba 8b.cpp
@@ -0,0 +1,89 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+void inputArray(int* array, int row, int col);
+void outputArray(int* array, int row, int col);
+int processArray(int* array, int row, int col);
+void outputResult(int result);
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int m[5][5];
+    int res;
+
+    inputArray((int*)m, 5, 5);//m-указатель на int
+    outputArray((int*)m, 5, 5);
+    res = processArray((int*)m, 5, 5);
+    outputResult(res);
+    
+    return 0;
+}
+
+void inputArray(int* array, int row, int col) // row-строка, col-столбец
+{
+    srand(time(NULL));
+    for (int i = 0; i < row; i++)
+    {
+        for (int i = 0; i < col; i++)
+        {
+            do
+            {
+                *array = rand() - RAND_MAX / 2;
+            }
+            while (*array == 0);
+            array++;
+        }
+    }
+}
+
+void outputArray(int* array, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int i = 0; i < col; i++)
+        {
+            printf("%8d ", *array);
+            array++;
+        }
+        printf("\n");
+    }
+}
+
+int processArray(int* array, int row, int col)
+{
+    int c = 0;
+    int minus = 0;
+    if (*array < 0)
+        minus = 1;
+
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int i = 0; i < col; i++)
+        {
+            if ((minus == 0) && (*array < 0))
+            {
+                c++;
+                minus = 1;
+            }
+            else if ((minus == 1) && (*array > 0))
+            {
+                c++;
+                minus = 0;
+            }
+            array++;
+        }
+    }
+    return c;
+}
+
+void outputResult(int result)
+{
+    printf("Элементы массива меняют знак %d раз\n", result);
+}
